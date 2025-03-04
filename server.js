4b4e9f890760c09465fb09be99a9378d57590e3a
CodzTitan <codztitan18@gmail.com>
2025-03-04 10:08:46 +0530

    Update server.js

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const app = express();
 // Middleware
 app.use(express.json());
 app.use(cors({
-  origin: 'https://your-netlify-app.netlify.app'
+  origin: 'https://moonlit-queijadas-c36cf5.netlify.app'
 }));
 
 // Connect to MongoDB
